8590305d9dcb1d2dd3ea7c9b06e13cadeffa02ea
DarkSummoner <jeonsooh@gmail.com>
2019-04-30 14:31:56 +0900

    커스텀 스크립트 템플릿 추가. string.IsNullOrEmpty()에 대한 확장 메소드 추가.

---
+++ UnityProject/FreeCell/Assets/ScriptTemplates/Editor/ScriptTemplateModifier.cs
@@ -0,0 +1,84 @@
+﻿//ref : https://forum.unity.com/threads/c-script-template-how-to-make-custom-changes.273191/#post-1806467
+
+using UnityEngine;
+using UnityEditor;
+using Summoner.Util.Extension;
+using System.Collections.Generic;
+
+namespace Summoner.Util.ScriptTemplates {
+
+	public class KeywordReplace : UnityEditor.AssetModificationProcessor {
+
+		private static string ReplaceContent( string content, string path ) {
+			content = content.Replace( "#PROJECTNAME#", PlayerSettings.productName );
+			content = content.Replace( "#NAMESPACE#", AsNameSpace( path ) );
+			return content;
+		}
+
+		private static string AsNameSpace( string path ) {
+			path = System.IO.Path.GetDirectoryName( path );
+			const string toFind = "Scripts";
+			var index = path.LastIndexOf( toFind );
+			path = path.Substring( index + toFind.Length );
+			path = path.Replace( '/', '.' );
+			return path;
+		}
+
+		public static void OnWillCreateAsset( string path ) {
+			path = path.Replace( ".meta", "" );
+
+			var file = File.Find( path );
+			if ( file == null ) {
+				return;
+			}
+
+			file.content = ReplaceContent( file.content, path );
+
+			AssetDatabase.Refresh();
+		}
+
+		private class File {
+			private readonly string path;
+
+			public string content {
+				get {
+					return System.IO.File.ReadAllText( path );
+				}
+
+				set {
+					System.IO.File.WriteAllText( path, value );
+				}
+			}
+
+			private File( string path ) {
+				this.path = path;
+			}
+
+			private static readonly IList<string> scriptExtensions = new [] { ".cs", ".js", ".boo" };
+			private static readonly string dataPath = Application.dataPath.Substring( 0, Application.dataPath.LastIndexOf( "Assets" ) );
+
+			public static File Find( string path ) {
+				var extension = System.IO.Path.GetExtension( path );
+				if ( extension == ".meta" ) {
+					path = path.Replace( ".meta", "" );
+					return Find( path );
+				}
+
+				if ( extension.IsNullOrEmpty() == true ) {
+					return null;
+				}
+
+				if ( scriptExtensions.Contains( extension ) == false ) {
+					return null;
+				}
+
+				path = dataPath + path;
+				if ( System.IO.File.Exists( path ) == false ) {
+					return null;
+				}
+
+				return new File( path );
+			}
+		}
+	}
+}
---
+++ UnityProject/FreeCell/Assets/Scripts/Util/Extension/StringExtension.cs
@@ -0,0 +1,8 @@
+
+namespace Summoner.Util.Extension {
+	public static class StringExtension {
+		public static bool IsNullOrEmpty( this string value ) {
+			return string.IsNullOrEmpty( value );
+		}
+	}
+}
